b3965325de72d5ae848e9f7c1505baa750a83cec
Ncookiez <ncukiert@gmail.com>
2022-05-16 20:57:48 -0300

    Fixed TVL (Thanks Underthesea)

--- src/lib/tvl.ts
+++ src/lib/tvl.ts
@@ -1,19 +1,20 @@
 
 // Imports:
 import weaver from 'weaverfi';
-import { minABI } from 'weaverfi/dist/ABIs.js';
-import type { Address } from 'weaverfi/dist/types';
+import type { Address, ABI } from 'weaverfi/dist/types';
 
 // Initializations:
 const ethTicket: Address = '0xdd4d117723C257CEe402285D3aCF218E9A8236E1';
 const polyTicket: Address = '0x6a304dFdb9f808741244b6bfEe65ca7B3b3A6076';
 const avaxTicket: Address = '0xB27f379C050f6eD0973A01667458af6eCeBc1d90';
 const decimals = 6;
+const ticketABI: ABI[] = [{ constant: true, inputs: [{ name: "_target", type: "uint64" }], name: "getTotalSupplyAt", outputs: [{ name: "", type: "uint256" }], type: "function" }];
 
 // Function to fetch PoolTogether V4 TVL:
 export const fetchTVL = async () => {
-  let ethTVL = parseInt(await weaver.ETH.query(ethTicket, minABI, 'totalSupply', []));
-  let polyTVL = parseInt(await weaver.POLY.query(polyTicket, minABI, 'totalSupply', []));
-  let avaxTVL = parseInt(await weaver.AVAX.query(avaxTicket, minABI, 'totalSupply', []));
+  let timestamp = (Date.now() / 1000).toFixed(0);
+  let ethTVL = parseInt(await weaver.ETH.query(ethTicket, ticketABI, 'getTotalSupplyAt', [timestamp]));
+  let polyTVL = parseInt(await weaver.POLY.query(polyTicket, ticketABI, 'getTotalSupplyAt', [timestamp]));
+  let avaxTVL = parseInt(await weaver.AVAX.query(avaxTicket, ticketABI, 'getTotalSupplyAt', [timestamp]));
   return (ethTVL + polyTVL + avaxTVL) / (10 ** decimals);
 }
